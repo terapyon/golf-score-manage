fe3391d525293bd4a4fdcd2105c715ac81715284
terapyon <Manabu TERADA>
2025-07-01 09:09:44 +0900

    fix lint error

--- src/test-types.d.ts
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom';
-
-declare namespace Vi {
-  interface Assertion<T = any> extends jest.Matchers<void, T> {}
-  interface AsymmetricMatchersContaining extends jest.AsymmetricMatchers {}
-}
